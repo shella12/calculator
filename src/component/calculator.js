70882629df4966233a40f444dc60a765eb66dc3f
shella12 <ayeshaarshad4567@gmail.com>
2022-11-29 12:50:02 +0500

    Add UI

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -9,7 +9,30 @@ class Calculator extends React.Component {
   render() {
     return (
       <div>
-        <h1>Calculator</h1>
+        <div className="calculator">
+          <div className="panel">0</div>
+          <div className="buttons">
+            <button type="button" className="calcBtn" value="AC">AC</button>
+            <button type="button" className="calcBtn" value="+/-">+/-</button>
+            <button type="button" className="calcBtn" value="%">%</button>
+            <button type="button" className="calcBtn orange" value="÷">÷</button>
+            <button type="button" className="calcBtn" value="7">7</button>
+            <button type="button" className="calcBtn" value="8">8</button>
+            <button type="button" className="calcBtn" value="9">9</button>
+            <button type="button" className="calcBtn orange" value="x">x</button>
+            <button type="button" className="calcBtn" value="4">4</button>
+            <button type="button" className="calcBtn" value="5">5</button>
+            <button type="button" className="calcBtn" value="6">6</button>
+            <button type="button" className="calcBtn orange" value="-">-</button>
+            <button type="button" className="calcBtn" value="1">1</button>
+            <button type="button" className="calcBtn" value="2">2</button>
+            <button type="button" className="calcBtn" value="3">3</button>
+            <button type="button" className="calcBtn orange" value="+">+</button>
+            <button type="button" className="calcBtn zero" value="0">0</button>
+            <button type="button" className="calcBtn" value=".">.</button>
+            <button type="button" className="calcBtn orange" value="=">=</button>
+          </div>
+        </div>
       </div>
     );
   }

9e449d0728ad08cc9d623ccd63fb80bfc5c68e5c
belalgamal46 <belalgamal00@gmail.com>
2022-12-07 15:01:51 +0200

    Add components.test file

---
+++ src/__tests__/components.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from '../components/home';
+import Calculator from '../components/calculator';
+import Quote from '../components/quote';
+
+describe('test suite for components', () => {
+  it('renders correctly home component', () => {
+    const tree = render(<Home />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('renders correctly calculator component', () => {
+    const tree = render(<Calculator />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('renders correctly quote component', () => {
+    const tree = render(<Quote />);
+    expect(tree).toMatchSnapshot();
+  });
+});
